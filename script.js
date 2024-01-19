b2de678f5a9a593c2efffacfdb93bd526d8fc192
sweepas <47362063+sweepas@users.noreply.github.com>
2024-01-19 14:22:15 +0000

    finished-task

--- script.js
+++ script.js
@@ -8,12 +8,33 @@ const observer = new IntersectionObserver(
   });
 },
 {
-   // threshold: 1,
-    rootMargin
+   threshold: 1,
+
 }
 );
+
+const theLastCardObserver = new IntersectionObserver(enteries =>{
+    const lastCard = enteries[0]
+    if(lastCard.isIntersecting) return
+    loadNewCard()
+    theLastCardObserver.unobserve(lastCard.target)
+    theLastCardObserver.observe(document.querySelector('.card:last-child'))
+},{})
+
+theLastCardObserver.observe(document.querySelector('.card:last-child'))
 
 observer.observe(cards[0]);
 cards.forEach((card) => {
   observer.observe(card);
 });
+
+const cardContainer = document.querySelector('.card-container')
+ function loadNewCard() {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement("div");
+        card.textContent = "New Card"
+        card.classList.add('card')
+        observer.observe(card)
+        cardContainer.append(card)
+    }
+ }
